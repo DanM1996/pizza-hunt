86572e2ba9694e5f6a26eabe5fb63f4302511937
Dan <bmdmartinez@gmail.com>
2021-09-14 12:01:24 -0400

    created controllers and routes for comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -2,10 +2,10 @@ const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
     pizzaName: {
-        type: String 
+        type: String
     },
     createdBy: {
-        type: String 
+        type: String
     },
     createdAt: {
         type: Date,
@@ -13,11 +13,28 @@ const PizzaSchema = new Schema({
     },
     size: {
         type: String,
-        default: 'Large' 
+        default: 'Large'
     },
-    toppings: []
-});
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
 
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
+})
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
