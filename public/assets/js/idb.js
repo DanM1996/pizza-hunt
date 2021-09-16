e1fa74f68b8ae54fe9f7792b1925fcbdcd499363
Dan <bmdmartinez@gmail.com>
2021-09-16 13:12:08 -0400

    indexdb works, also added functionality for the app to auto push to the website once it comes back online

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -5,7 +5,7 @@ let db;
 const request = indexedDB.open('pizza_hunt', 1);
 
 // runs when a database with more data is stored, in essence upgrading from version 1 to 2
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     // save a reference to the database
     const db = event.target.result;
     // create an object store (table) called 'new_pizza', set it to have an auto incrementing primary key of sorts
@@ -13,18 +13,18 @@ request.onupgradeneeded = function(event) {
 };
 
 // when the connection is successful
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     // when database is sucessfully created with its object store(table) (from upgrade event above) or simply established a connection, save reference to db in global variable 
     db = event.target.result;
 
     // check if app is online, if yes run uploadPizza() function to send all local db data to api
     if (navigator.online) {
-        // we haven't created this yet, but we will soon
-        // uploadPizza();
+        // run the uploadPizza function
+        uploadPizza();
     }
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
     // log the error
     console.log(event.target.errorCode);
 }
@@ -34,10 +34,56 @@ request.onerror = function(event) {
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions
     const transaction = db.transaction(['new_pizza'], 'readwrite');
-    
+
     // access the object store for 'new_pizza'
     const pizzaObjectStore = transaction.objectStore('new_pizza');
 
     // add record(new data) to your store with add method
     pizzaObjectStore.add(record);
 }
+
+// data being pushed to the real database from idb once internet connection is restored
+function uploadPizza() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access your object store
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    // get all records from the store and set to a variable
+    const getAll = pizzaObjectStore.getAll();
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/pizzas', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_pizza'], 'readwrite');
+                    // access the new_pizza object store
+                    const pizzaObjectStore = transaction.objectStore('new_pizza');
+                    // clear all items in your store
+                    pizzaObjectStore.clear();
+
+                    alert('All saved pizza has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadPizza);
